559c2c16bbc583791dccbc63bb6d360d75527a08
edivaner <edivanerfernandes18@gmail.com>
2023-03-12 22:02:46 -0300

    adicioandno teste jest [teste]

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,10 @@
+
+
+describe("Criar Categoria", () => {
+    it("Espero que 2 + 2 seja 4", () => {
+        const soma = 2 + 2;
+        const result = 4;
+
+        expect(soma).toBe(result);
+    });
+});
